2ebe78bab635de3ad7de0765d506089322c8ce2e
Asiya1330 <asiya.batool987@gmail.com>
2023-07-23 14:47:38 +0500

    initial-commit

---
+++ svgs/robot-shadow.js
@@ -0,0 +1,16 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="336" height="506" viewBox="0 0 336 506" fill="none">
+  <g opacity="0.1" filter="url(#filter0_f_1_3)">
+    <path d="M158.08 469.159C122.08 467.159 109 412 105 320C103.307 296 115 176 105 206C94.9998 236 34.0802 319.159 36.0802 269.159C38.0802 219.159 72.0802 251.159 90.0802 143.159C108.08 35.1585 172.08 25.1586 200.08 43.1586C228.08 61.1586 242.08 115.159 238.08 157.159C234.08 199.159 240.08 251.159 276.08 299.159C312.08 347.159 306.08 379.159 266.08 381.159C226.08 383.159 186.08 237.159 172.08 251.159C158.08 265.159 144.08 311.159 172.08 359.159C200.08 407.159 194.08 471.159 158.08 469.159Z" fill="url(#paint0_linear_1_3)"/>
+  </g>
+  <defs>
+    <filter id="filter0_f_1_3" x="0.0320435" y="0" width="335.863" height="505.204" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+      <feGaussianBlur stdDeviation="18" result="effect1_foregroundBlur_1_3"/>
+    </filter>
+    <linearGradient id="paint0_linear_1_3" x1="167.947" y1="155" x2="167.947" y2="559" gradientUnits="userSpaceOnUse">
+      <stop offset="0.0693069"/>
+      <stop offset="1" stop-opacity="0"/>
+    </linearGradient>
+  </defs>
+</svg>
